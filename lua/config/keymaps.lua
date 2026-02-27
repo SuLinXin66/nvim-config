4c5c2af6b8f36c27b4801976ce05142923cbf20b
夏夜 <sulinxin66@gmail.com>
2026-02-27 19:35:14 +0800

    pref: 优化删除时候的粘贴板被记录

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,3 +11,18 @@ wk.add({
 local keymap = vim.keymap.set
 keymap("i", "jj", "<Esc>", { desc = "Exit insert mode with jj" })
 keymap("i", "jk", "<Esc>", { desc = "Exit insert mode with jk" })
+
+-- 目标：
+-- 1) y 仍然走系统剪贴板（LazyVim 的 clipboard=unnamedplus 保持不动）
+-- 2) d / c 系列默认不污染剪贴板（写入黑洞寄存器）
+-- Delete: do not affect registers/clipboard
+keymap({ "n", "x" }, "d", '"_d', { desc = "Delete (black hole)" })
+keymap({ "n", "x" }, "D", '"_D', { desc = "Delete line-end (black hole)" })
+
+-- Change: do not affect registers/clipboard
+keymap({ "n", "x" }, "c", '"_c', { desc = "Change (black hole)" })
+keymap({ "n", "x" }, "C", '"_C', { desc = "Change line-end (black hole)" })
+
+-- 可选：x / X 也不污染（通常本来就是小删除，但开了 unnamedplus 后也可能影响剪贴板）
+keymap({ "n", "x" }, "x", '"_x', { desc = "Char delete (black hole)" })
+keymap({ "n", "x" }, "X", '"_X', { desc = "Backspace delete (black hole)" })
