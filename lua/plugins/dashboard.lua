66cd833c61eb28ee0a43841810e8be440cdbba9a
夏夜 <sulinxin66@gmail.com>
2026-03-06 23:57:17 +0800

    perf: 修改启动页logo

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          header = [[
+  /\_/\ 
+ ( -.- )
+ > ^ <
+
+   NO BUGS TODA
+]],
+        },
+      },
+    },
+  },
+}
