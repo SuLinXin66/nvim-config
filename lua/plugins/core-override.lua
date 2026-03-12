26433392dd3556e2d5c8e410a98b790e59fd73e4
夏夜 <sulinxin66@gmail.com>
2026-03-13 01:39:41 +0800

    perf: 优化进入preview的快捷键

--- lua/plugins/core-override.lua
+++ lua/plugins/core-override.lua
@@ -5,7 +5,14 @@ return {
       win = {
         input = {
           keys = {
-            ["<C-w>p"] = { "focus_preview", mode = { "n", "i" } },
+            ["<C-w>l"] = { "focus_preview", mode = { "n", "i" } },
+            ["<C-l>"] = { "focus_preview", mode = { "n", "i" } },
+          },
+        },
+        preview = {
+          keys = {
+            ["<C-w>h"] = { "focus_input", mode = { "n" } },
+            ["<C-h>"] = { "focus_input", mode = { "n" } },
           },
         },
       },
