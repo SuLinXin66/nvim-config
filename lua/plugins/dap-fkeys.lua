f3530ccb8e66fcba12740f8ace463f4eb32cfe1d
夏夜 <sulinxin66@gmail.com>
2026-03-14 22:50:05 +0800

    perf: 优化进入dap调试F5的逻辑

--- lua/plugins/dap-fkeys.lua
+++ lua/plugins/dap-fkeys.lua
@@ -1,3 +1,6 @@
+local logger = require("tools.logger")
+local has_started_once = false
+
 return {
   {
     "mfussenegger/nvim-dap",
@@ -7,9 +10,6 @@ return {
         function()
           local dap = require("dap")
 
-          -- 记录当前 Neovim 这次会话里，是否已经至少启动过一次 DAP
-          vim.g._dap_has_started_once = vim.g._dap_has_started_once or false
-
           -- 1. 如果当前已经有活动调试会话
           if dap.session() then
             dap.continue()
@@ -18,7 +18,7 @@ return {
 
           -- 2. 当前没有活动调试会话
           -- 如果之前已经启动过一次，就直接 Run Last
-          if vim.g._dap_has_started_once then
+          if has_started_once then
             dap.run_last()
             return
           end
@@ -26,7 +26,7 @@ return {
           -- 3. 第一次启动，没有 last，就走 Continue
           -- Continue 在无 session 时会按当前 filetype 配置启动调试
           dap.continue()
-          vim.g._dap_has_started_once = true
+          has_started_once = true
         end,
         desc = "DAP Run Last",
       },
