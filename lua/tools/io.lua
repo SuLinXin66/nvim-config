9f79200942c0e9fa4dcd61c81f4ff25fb3552f10
夏夜 <sulinxin66@gmail.com>
2026-03-15 23:34:37 +0800

    perf: 优化io工具

--- lua/tools/io.lua
+++ lua/tools/io.lua
@@ -1,12 +1,27 @@
+local function read_file(filepath, allow_empty_content)
+  allow_empty_content = allow_empty_content or false
+  local ok, content = pcall(vim.fn.readfile, filepath)
+  if not ok then
+    return nil, "Failed to read file: " .. filepath
+  end
+
+  if not allow_empty_content and #content == 0 then
+    return nil, "File is empty: " .. filepath
+  end
+
+  return content
+end
+
 local M = {}
 
-function M.read_json_to_table(file_path)
-  local content = vim.fn.readfile(file_path)
-  if not content then
-    return nil, "Failed to read file: " .. file_path
+function M.read_json_to_table(filepath)
+  local content, err = read_file(filepath)
+  if err ~= nil or content == nil then
+    return nil, err
   end
 
   local json_str = table.concat(content, "\n")
+
   local ok, result = pcall(vim.fn.json_decode, json_str)
   if not ok then
     return nil, "Failed to decode JSON: " .. result
@@ -15,10 +30,10 @@ function M.read_json_to_table(file_path)
   return result
 end
 
-function M.read_env_file_to_table(file_path)
-  local content = vim.fn.readfile(file_path)
-  if not content then
-    return nil, "Failed to read file: " .. file_path
+function M.read_env_file_to_table(filepath)
+  local content, err = read_file(filepath)
+  if err ~= nil or content == nil then
+    return nil, err
   end
 
   local result = {}
@@ -30,6 +45,20 @@ function M.read_env_file_to_table(file_path)
   end
 
   return result
+end
+
+function M.get_cwd_file_path()
+  local cwd = vim.fn.getcwd()
+  return cwd:gsub(":", ""):gsub("[/\\]", "%%")
+end
+
+function M.get_state_file_path(tag, filename)
+  tag = tag or ""
+  filename = filename or M.get_cwd_file_path()
+  if tag ~= "" then
+    return vim.fs.joinpath(vim.fn.stdpath("state"), tag, filename)
+  end
+  return vim.fs.joinpath(vim.fn.stdpath("state"), filename)
 end
 
 return M
